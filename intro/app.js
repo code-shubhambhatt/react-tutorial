6c95105c812441266d0518e044ab1967fc251127
code-shubhambhatt <shubhambhatt1000@gmail.com>
2025-12-02 12:38:32 +0530

    pushing 01-folder code to github

---
+++ intro/app.js
@@ -0,0 +1,6 @@
+const arr = [20,30 ,2,221,65];
+const user = 'Shubam';
+export const pass = '123123';
+
+export default user ;
+
---
+++ intro/script.js
@@ -0,0 +1,4 @@
+import a from './app.js'
+import {pass} from './app.js'
+console.log(pass)
+console.log(a)
